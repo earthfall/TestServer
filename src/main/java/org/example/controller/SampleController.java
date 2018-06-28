9c8b616a797002743e2527347f11470ec6d0d8ee
Young Ik Cho <youngikcho@gmail.com>
2018-06-28 23:30:35 +0900

    Add apidoc support
    Signed-off-by: Young Ik Cho <youngikcho@gmail.com>

--- src/main/java/org/example/controller/SampleController.java
+++ src/main/java/org/example/controller/SampleController.java
@@ -27,6 +27,31 @@ public class SampleController {
     @Autowired
     private DeviceNotificationService deviceNotificationService;
 
+    /**
+     * @api {get} /hello/:name Request User information
+     * @apiName hello
+     * @apiGroup User
+     *
+     * @apiParam {Number} id Users unique ID.
+     *
+     * @apiSuccess {String} firstname Firstname of the User.
+     * @apiSuccess {String} lastname  Lastname of the User.
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "firstname": "John",
+     *       "lastname": "Doe"
+     *     }
+     *
+     * @apiError UserNotFound The id of the User was not found.
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "error": "UserNotFound"
+     *     }
+     */
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
         return appConfig.getGreeting() + ' ' + name + "!";
