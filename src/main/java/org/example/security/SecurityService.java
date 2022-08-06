ea2b6a3710ffc53a752549ae0c9d6781133fa57b
Young Ik Cho <2497650+earthfall@users.noreply.github.com>
2022-08-06 22:07:47 +0900

    add event/publisher for webflux
    Signed-off-by: Young Ik Cho <2497650+earthfall@users.noreply.github.com>

--- src/main/java/org/example/security/SecurityService.java
+++ src/main/java/org/example/security/SecurityService.java
@@ -1,14 +1,20 @@
 package org.example.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class SecurityService {
 
+    private final SecurityEventPublisher securityEventPublisher;
+
     Mono<String> testHello() {
-        return Mono.just("This hello");
+        return Mono.just("This hello")
+            .doOnSuccess(it -> log.debug("triggered"))
+            .doOnSuccess(it -> securityEventPublisher.publishCustomEvent("done"));
     }
 }
