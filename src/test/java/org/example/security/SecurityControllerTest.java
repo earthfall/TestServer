2c88e089afc056b7e8127eb6984d2d5d9d35ad6f
Young Cho <2497650+earthfall@users.noreply.github.com>
2022-08-06 22:20:55 +0900

    fix unit test failure (#3)
    Signed-off-by: Young Ik Cho <2497650+earthfall@users.noreply.github.com>

--- src/test/java/org/example/security/SecurityControllerTest.java
+++ src/test/java/org/example/security/SecurityControllerTest.java
@@ -2,18 +2,18 @@ package org.example.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.test.StepVerifier;
 
-@WebFluxTest(SecurityController.class)
-@Import({SecurityService.class, SecurityEvaluator.class, UserService.class})
+@SpringBootTest
+@AutoConfigureWebTestClient
 public class SecurityControllerTest {
 
     @Autowired
-    private WebTestClient webClient;
+    WebTestClient webClient;
 
     @Test
     public void testEvaluator() {
