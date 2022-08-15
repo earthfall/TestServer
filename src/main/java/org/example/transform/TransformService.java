0381ebf1af38056ce8be119e01bf308455e1652c
Young Ik Cho <2497650+earthfall@users.noreply.github.com>
2022-08-15 23:52:41 +0900

    better transform role separation
    Signed-off-by: Young Ik Cho <2497650+earthfall@users.noreply.github.com>

--- src/main/java/org/example/transform/TransformService.java
+++ src/main/java/org/example/transform/TransformService.java
@@ -1,6 +1,8 @@
 package org.example.transform;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -8,36 +10,23 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Log4j2
+@RequiredArgsConstructor
 @Service
 public class TransformService {
 
     private final Map<Class<?>, Set<TransformItem>> classCache = new ConcurrentHashMap<>();
 
+    private final Transformer transformer;
+
     public <T extends Transformable<T>> T transform(T source) throws IllegalAccessException {
-        return transformData(source.deepCopy());
-    }
-
-    private <T extends Transformable<T>> T transformData(T source) throws IllegalAccessException {
-        var transforms = classCache.computeIfAbsent(source.getClass(), key -> {
-            var infos = Transformable.transformDataInfo(key);
-            log.info("Transform initialized for {}", key);
-            return infos;
-        });
-        return transformData(source, transforms);
-    }
-
-    private <T extends Transformable<T>> T transformData(T source, Set<TransformItem> transforms) throws IllegalAccessException {
-        for (TransformItem item : transforms) {
-            var varHandle = item.getVarHandle();
-            var object = varHandle.get(source);
-
-            if (object instanceof String && item.isTransformDataAnnotation()) {
-                varHandle.set(source, Transformable.transformString((String) object));
-            } else if (object instanceof Transformable) {
-                varHandle.set(source, transformData((Transformable) object));
-            }
-        }
-
-        return source;
+        return source.deepCopy()
+            .transformData(
+                clazz -> classCache.computeIfAbsent(clazz, key -> {
+                    var infos = TransformDataParser.transformDataInfo(key);
+                    log.info("Transform initialized for {}", key);
+                    return infos;
+                }),
+                transformer::transformString
+            );
     }
 }
