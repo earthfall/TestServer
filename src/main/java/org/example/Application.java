868c48f00d5232b6e2fcfe3865cd9b4923bc7a9e
Young Ik Cho <2497650+earthfall@users.noreply.github.com>
2022-09-01 13:54:11 +0900

    register aws bean

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import org.example.config.AppConfig;
+import org.example.config.AwsProperties;
 import org.example.config.FirebaseConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({AppConfig.class, FirebaseConfig.class})
+@EnableConfigurationProperties({AppConfig.class, FirebaseConfig.class, AwsProperties.class})
 public class Application {
 
     public static void main(String[] args) {
