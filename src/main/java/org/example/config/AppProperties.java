9eb8dd6e11178597e8b3147c84cae87cd4fa6e34
Young Cho <2497650+earthfall@users.noreply.github.com>
2023-03-04 23:23:50 +0900

    spring boot 3.0.4 (#13)
    Signed-off-by: Young Ik Cho <2497650+earthfall@users.noreply.github.com>

--- src/main/java/org/example/config/AppProperties.java
+++ src/main/java/org/example/config/AppProperties.java
@@ -1,9 +1,7 @@
 package org.example.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
-@ConstructorBinding
 @ConfigurationProperties(prefix = "test")
 public record AppProperties(
     int id,
--- src/main/java/org/example/config/AwsProperties.java
+++ src/main/java/org/example/config/AwsProperties.java
@@ -1,24 +1,20 @@
 package org.example.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties(prefix = "amazon")
-@ConstructorBinding
 public record AwsProperties(
     Aws aws,
     DynamoDb dynamoDb
 ) {
 }
 
-@ConstructorBinding
 record Aws(
     String accessKey,
     String secretKey
 ) {
 }
 
-@ConstructorBinding
 record DynamoDb(
     String endpoint,
     String region
