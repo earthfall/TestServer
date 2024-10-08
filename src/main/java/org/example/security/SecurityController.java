497664927a3561928f38dcb0ed6dd55df749970d
Young Cho <2497650+earthfall@users.noreply.github.com>
2024-10-09 00:16:27 +0900

    add protobuf controller (#16)
    Signed-off-by: Young Ik Cho <2497650+earthfall@users.noreply.github.com>

--- src/main/java/org/example/security/SecurityController.java
+++ src/main/java/org/example/security/SecurityController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.example.aop.annotations.HasPermission;
 import org.example.aop.annotations.User;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import org.example.security.model.Message;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -40,5 +43,14 @@ public class SecurityController {
     public Mono<String> helloAop(@User @PathVariable String name) {
         log.debug("Received aop request with " + name);
         return securityService.testHello();
+    }
+
+    @GetMapping(value = "/permission-valid/{name}", produces = MediaType.APPLICATION_PROTOBUF_VALUE)
+    public Mono<Message> helloAopProto(@User @PathVariable String name) {
+        log.debug("Received protobuf request with " + name);
+        return securityService.testHello()
+            .map(it ->
+                Message.newBuilder().setName(it).build()
+            );
     }
 }
